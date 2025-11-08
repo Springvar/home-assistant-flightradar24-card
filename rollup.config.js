db1405dff7f83464aa4c6282f897e1bf4ddc3faa
Ivar Bonsaksen <ivar.bonsaksen@gmail.com>
2025-11-08 14:07:33 +0100

    refactor(build): use `home-assistant-flightradar24-card.js` as output filename in build, release, and metadata
    - Update rollup config to emit `dist/home-assistant-flightradar24-card.js`
    - Update HACS manifest, build, and release workflow references to new filename

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'flightradar24-card.js',
-  output: {
-    file: 'dist/flightradar24-card.js',
-    format: 'es',
-    sourcemap: false,
-  },
-  plugins: [
-    resolve(),
-    commonjs(),
-  ],
+    input: 'flightradar24-card.js',
+    output: {
+        file: 'dist/home-assistant-flightradar24-card.js',
+        format: 'es',
+        sourcemap: false
+    },
+    plugins: [resolve(), commonjs()]
 };
